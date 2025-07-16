e433f9bebebf2fde5d8e94aff9421c1131f60495
31pranjal <bhawsarpranjal0@gmail.com>
2025-07-16 22:46:19 +0530

    Add files via upload

---
+++ ruby
@@ -0,0 +1,1 @@
+https://i.pinimg.com/originals/87/20/32/872032c6f1773e927b9c29ad07d849b2.jpg
---
+++ script.js
@@ -0,0 +1,2 @@
+// You can add animations or interactive features here
+console.log("Welcome to Preeti Restaurant website!");
